85786a0ba3187b9b2f6ed2d3e4884df73312b837
Balaakay <blakedallas36@gmail.com>
2025-03-22 17:34:26 -0500

    added gts and created the script that will move everyghing to the correct place for updating configs. The gts function does not yet handle the different OS's I may be using so make sure to add conditional logic based on OS

---
+++ moveConfigs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script needs to take the files/directories in the .dotfiles path and move
+# them to the correct place for .config paths according to each tool/application.
+#
+# This should also be based on OS. I would like to keep this same script for MacOS
+# and for Linux.
