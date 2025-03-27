5ad5ca80bc026c2d928b7cb343355a8e5ccaf4a1
Balaakay <blakedallas36@gmail.com>
2025-03-26 22:39:48 -0500

    rewrote gts.sh and made harpoon respect me deleting lines inside the popup

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,6 +1,9 @@
 local harpoon = require("harpoon")
 
-harpoon:setup()
+harpoon:setup({
+    save_on_toggle = true,
+  }
+)
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
