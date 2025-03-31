43d31a2cafb65dd544e35cbbb1b2ab947546a6c3
Balaakay <blakedallas36@gmail.com>
2025-03-31 15:35:29 -0500

    redoing colors

--- nvim/lua/balaakay/packer.lua
+++ nvim/lua/balaakay/packer.lua
@@ -12,19 +12,13 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use { "rose-pine/neovim", as = "rose-pine",
-  config = function()
-	  vim.cmd('colorscheme rose-pine')
-	end
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
   }
-
-    use {
-        'nvim-treesitter/nvim-treesitter',
-        run = function()
-            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-            ts_update()
-        end,
-    }
 
   use {'nvim-treesitter/playground'}
   use {
